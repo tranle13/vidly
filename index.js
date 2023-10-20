5368f4ca18472a18b774bcf2b4f66ae38dc05baf
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-19 22:38:58 -0400

    Add new customer model & routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
-const genres = require("./routes/genres");
 const express = require("express");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const app = express();
 
 mongoose
@@ -11,6 +11,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
