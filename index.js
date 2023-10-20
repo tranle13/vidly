a1aca36ebbe4adf3adb66ed1adbc7fb760c9b319
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-19 22:33:06 -0400

    Add express, genre model and modify routes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require("mongoose");
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(() => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
 app.use("/api/genres", genres);
